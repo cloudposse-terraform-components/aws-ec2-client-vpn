09a800f5d696dcc1819d101702ef7c1fad6fe058
Igor Rodionov <goruha@gmail.com>
2024-11-02 19:18:00 +0100

    Initial commit

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,25 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "vpn_endpoint_arn" {
+  value       = module.ec2_client_vpn.vpn_endpoint_arn
+  description = "The ARN of the Client VPN Endpoint Connection."
+}
+
+output "vpn_endpoint_id" {
+  value       = module.ec2_client_vpn.vpn_endpoint_id
+  description = "The ID of the Client VPN Endpoint Connection."
+}
+
+output "vpn_endpoint_dns_name" {
+  value       = module.ec2_client_vpn.vpn_endpoint_dns_name
+  description = "The DNS Name of the Client VPN Endpoint Connection."
+}
+
+output "client_configuration" {
+  value       = module.ec2_client_vpn.client_configuration
+  description = "VPN Client Configuration file (.ovpn) contents that can be imported into AWS client vpn"
+}
+
+output "full_client_configuration" {
+  value       = module.ec2_client_vpn.full_client_configuration
+  description = "Client configuration including client certificate and private key for mutual authentication"
+  sensitive   = true
 }
